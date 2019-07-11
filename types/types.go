3144966074520dc83133fcfb28e0766167408cbe
Harshal Patil <harshal.patil@in.ibm.com>
2019-07-11 19:53:20 +0530

    Initial commit to support image decryption
    Signed-off-by: Harshal Patil <harshal.patil@in.ibm.com>

--- types/types.go
+++ types/types.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/containers/image/docker/reference"
 	"github.com/opencontainers/go-digest"
-	"github.com/opencontainers/image-spec/specs-go/v1"
+	v1 "github.com/opencontainers/image-spec/specs-go/v1"
 )
 
 // ImageTransport is a top-level namespace for ways to to store/load an image.
@@ -489,8 +489,8 @@ type SystemContext struct {
 	DockerInsecureSkipTLSVerify OptionalBool
 	// if nil, the library tries to parse ~/.docker/config.json to retrieve credentials
 	DockerAuthConfig *DockerAuthConfig
-        // if not nil, decryption keys will be used to decrypt container image
-        DecryptParams []string
+	// if not nil, decryption keys will be used to decrypt container image
+	DecryptParams []string
 	// if not "", an User-Agent header is added to each request when contacting a registry.
 	DockerRegistryUserAgent string
 	// if true, a V1 ping attempt isn't done to give users a better error. Default is false.
